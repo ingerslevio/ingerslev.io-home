bb06eea38e437a2850e5df6c1411984eb85fe9fe
Emil Ingerslev <emil@ingerslev.io>
2025-10-05 21:01:34 +0200

    lets try

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ingerslevio.github.io",
+  base: "/ingerslev.io-home",
   i18n: {
     locales: ["en", "da"],
     defaultLocale: "da",
