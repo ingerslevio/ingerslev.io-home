9f3148287bb9587ea5fe39b615bfc05ddd6aa3b0
Emil Ingerslev <emil@ingerslev.io>
2025-10-05 21:09:09 +0200

    fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ingerslevio.github.io",
-  base: "/ingerslev.io-home",
+  base: "/",
   i18n: {
     locales: ["en", "da"],
     defaultLocale: "da",
